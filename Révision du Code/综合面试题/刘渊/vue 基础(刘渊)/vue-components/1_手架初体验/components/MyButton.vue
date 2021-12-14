df08266ed95a799ab2a9feb29e03dd0fc4777e90
FondKevinZhao <443403633@qq.com>
2021-12-14 21:30:14 +0800

    updated

---
+++ Révision du Code/综合面试题/刘渊/vue 基础(刘渊)/vue-components/1_手架初体验/components/MyButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>{{ count }}</p>
+    <button @click="myCount">点击我增加{{ count }}次</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "my-button",
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    myCount() {
+      this.count++;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- Révision du Code/综合面试题/刘渊/vue 基础(刘渊)/vue-components/src/main.js
+++ Révision du Code/综合面试题/刘渊/vue 基础(刘渊)/vue-components/src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 
 Vue.config.productionTip = false
 
-// /* eslint-disable no-unused-vars */
-var a;
-
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  render: h => h(App)
+})
