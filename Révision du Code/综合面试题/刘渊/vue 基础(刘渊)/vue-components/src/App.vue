658cd0985095a2c96c92b170ed0eb4ff133cc196
FondKevinZhao <443403633@qq.com>
2021-12-17 21:18:06 +0800

    updated

--- Révision du Code/综合面试题/刘渊/vue 基础(刘渊)/vue-components/src/App.vue
+++ Révision du Code/综合面试题/刘渊/vue 基础(刘渊)/vue-components/src/App.vue
@@ -1,10 +1,32 @@
 <template>
-  
+  <div>
+    <Header></Header>
+    <div class="container">
+      <Add></Add>
+      <List :coms="comments"></List>
+    </div>
+  </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
+import Add from './components/Add.vue'
+import List from './components/List.vue'
 export default {
-
+  components: {
+    Header,
+    Add,
+    List
+  },
+  data() {
+    return {
+      comments: [
+        {id: 1, content: 'vue牛逼',username:'杨幂'},
+        {id: 2, content: 'vue可以',username:'赵丽颖'},
+        {id: 3, content: 'vue不错',username:'戚薇'},
+      ]
+    }
+  }
 }
 </script>
 
