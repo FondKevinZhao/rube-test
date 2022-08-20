2aca20e8fffb0b2a77708500ca6646f14aaac1b5
FondKevinZhao <443403633@qq.com>
2022-08-21 02:02:01 +0800

    对内容做了调整

--- Révision du Code/06-TypeScript/03-杨洪波/03-类型注解/index.ts
+++ Révision du Code/06-TypeScript/03-杨洪波/03-类型注解/index.ts
@@ -20,3 +20,13 @@
   // 智能的错误提示信息
   console.log(showMsg(msg)); // 调用会报错
 })() */
+
+
+// type 使用：type叫作类型别名，相当于是给某种类型取了一个外号
+type List = object[]
+type Nubmerable = number | string
+
+interface ListModel {
+  total: Nubmerable
+  list: List
+}
