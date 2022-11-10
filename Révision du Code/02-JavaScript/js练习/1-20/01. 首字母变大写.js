c6b4d61699bbef4ca9f13bc8250a52454f07d608
FondKevinZhao <443403633@qq.com>
2022-11-10 14:56:18 +0800

    对内容做了调整

--- Révision du Code/02-JavaScript/js练习/1-20/01. 首字母变大写.js
+++ Révision du Code/02-JavaScript/js练习/1-20/01. 首字母变大写.js
@@ -7,6 +7,7 @@ function getCamelCase(str) {
       // return arr[0]
       return item
     } else {
+      // charAt() 返回指定位置的字符。如：str.charAt(1); 返回字符串中下标为1的字符
       return item.charAt(0).toUpperCase() + item.slice(1)
     }
   }).join('')
